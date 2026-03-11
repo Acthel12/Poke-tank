36af95a9dbeee0bb8c7970ac7c410d79faa8e872
Gabriel Carneiro <gecgpro12@gmail.com>
2026-03-10 21:37:37 -0400

    Añadido la base del deltaTime

---
+++ Poke-tank/MotorTiempo.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+public static class MotorTiempo
+{
+	private static Stopwatch cronometro = new Stopwatch();
+
+	public static float DeltaTime { get; private set; }
+
+	public static void Iniciar()
+	{
+		cronometro.Start();
+    }
+	public static void Actualizar()
+	{
+		DeltaTime = (float)cronometro.Elapsed.TotalSeconds;
+
+		cronometro.Restart();
+    }
+}
